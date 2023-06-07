432bb544df4f6c015534b3813ff7a3ddcc4a7b7d
zhilin he <zhilinhe@zhilins-MacBook-Pro.local>
2023-06-07 19:20:31 +0800

    deploy

--- blueprints/dataApi/__init__.py
+++ blueprints/dataApi/__init__.py
@@ -2,10 +2,12 @@ import datetime
 from pprint import pprint
 
 from blueprints.dataApi.controllers import DataController
-from flask import Blueprint, request
+from flask import Blueprint, make_response, request, current_app
 from utils import iso_ts
 from utils.response_tools import (SuccessDataResponse, ArgumentExceptionResponse)
 from flask import jsonify
+from datetime import timedelta
+from functools import update_wrapper
 
 bp = Blueprint('data_api', __name__, url_prefix='/api/data_api')
 
@@ -15,9 +17,13 @@ def fetch_corpus():
     page = args.get('page')
     if page > 0:
         data = DataController.fetch_corpus_data(page = page)
-        return SuccessDataResponse(data)
+        response = SuccessDataResponse(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     else:
-        return ArgumentExceptionResponse(msg='wrong page index.')
+        response = ArgumentExceptionResponse(msg='wrong page index.')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
 @bp.route('fetch_subject', methods=['POST'])
 def fetch_subject():
@@ -25,9 +31,13 @@ def fetch_subject():
     page = args.get('page')
     if page > 0:
         data = DataController.fetch_subject_data(page = page)
-        return SuccessDataResponse(data)
+        response = SuccessDataResponse(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     else:
-        return ArgumentExceptionResponse(msg='wrong page index.')
+        response = ArgumentExceptionResponse(msg='wrong page index.')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 @bp.route('test', methods=['GET'])
 def test():
     return 'hello'
